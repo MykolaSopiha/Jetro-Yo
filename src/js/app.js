8549c7d0a084c88b609872b35b2c99a008f0d91e
Sopiha-MV94 <niksop94@gmail.com>
2017-07-09 22:52:45 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".nav__mobile").html($(".nav__main").html());
+    $(".nav__trigger span").click(function(){
+        if ($("nav.nav__mobile ul").hasClass("expanded")) {
+            $("nav.nav__mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+        } else {
+            $("nav.nav__mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+        }
+    });
+});
